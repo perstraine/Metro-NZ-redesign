70dac87acc7cd75ce8f693f97dd2a2f377770bab
Simon Li <li.simon021@gmail.com>
2022-09-20 04:07:38 +1200

    2/4 sections - who we are section completed

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,6 +1,7 @@
-import Hero from "../components/shared/hero/Hero";
 import React from "react";
 import Nav from "../components/shared/nav/Nav";
+import Hero from "../components/hero/Hero";
+import WhoAreWe from "../components/who are we/whoAreWe";
 import NewsletterSignup from "../components/NewsletterSignup/NewsletterSignup";
 import Footer from "../components/shared/footer/Footer";
 
@@ -9,6 +10,7 @@ export default function Home() {
 		<>
 			<Nav />
 			<Hero />
+			<WhoAreWe />
 			<NewsletterSignup />
 			<Footer />
 		</>
