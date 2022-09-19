21c8a3c27f477fa70740ea57ac5188a24e625b62
annalittler <77206754+annalittler@users.noreply.github.com>
2022-09-19 15:02:02 +1200

    starting search page

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ front/src/pages/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../components/shared/nav/Nav";
+import SearchImg from "../components/SearchImg/SearchImg";
+import Footer from "../components/shared/footer/Footer";
+
+export default function Search() {
+  return (
+    <>
+      <Nav />
+      <SearchImg />
+      <Footer />
+    </>
+  );
+}
