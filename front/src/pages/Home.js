67a91b1910f7cf61425341cc349d133a24c07835
perstraine <41206120+perstraine@users.noreply.github.com>
2022-09-20 14:17:12 +1200

    added community section

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -4,6 +4,7 @@ import NewsletterSignup from "../components/NewsletterSignup/NewsletterSignup";
 import Footer from "../components/shared/footer/Footer";
 import Testimonials from "../components/home/testimonials/Testimonials";
 import Imgcarousel from "../components/home/imgcarousel/Imgcarousel";
+import Community from "../components/home/community/Community";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Nav />
       <Testimonials />
       <Imgcarousel />
+      <Community/>
       <NewsletterSignup />
       <Footer />
     </>
