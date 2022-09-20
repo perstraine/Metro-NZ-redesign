2861bf2105e8620551d460a009b7a0badeac8537
perstraine <41206120+perstraine@users.noreply.github.com>
2022-09-20 13:22:51 +1200

    image carousel done

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -3,12 +3,14 @@ import Nav from "../components/shared/nav/Nav";
 import NewsletterSignup from "../components/NewsletterSignup/NewsletterSignup";
 import Footer from "../components/shared/footer/Footer";
 import Testimonials from "../components/home/testimonials/Testimonials";
+import Imgcarousel from "../components/home/imgcarousel/Imgcarousel";
 
 export default function Home() {
   return (
     <>
       <Nav />
-      <Testimonials/>
+      <Testimonials />
+      <Imgcarousel />
       <NewsletterSignup />
       <Footer />
     </>
