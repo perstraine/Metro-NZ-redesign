c5ae3c987c476159c08b3d7dbd5f12f1ca5c3980
perstraine <41206120+perstraine@users.noreply.github.com>
2022-09-21 19:35:02 +1200

    Gallery works, fetches images based on url

--- front/src/components/listing/gallery/Gallery.js
+++ front/src/components/listing/gallery/Gallery.js
@@ -1,13 +1,59 @@
-import styles from './Gallery.module.css'
-import axios from 'axios';
+import styles from "./Gallery.module.css";
+import back from "./assets/back.png";
+import { useState } from "react";
 
-
-export default function Gallery({ result, listing }) {
-    return (
-        <>{result && <>
-        <div>Result:</div>
-        <div></div></>
-        }
-      </>
-    );
+export default function Gallery({ images }) {
+  const [index, setIndex] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <>
+      <div id={styles.outerContainer}>
+        <div id={styles.innerContainer}>
+          <div id={styles.back}>
+            <div>
+              <img src={back} alt="arrow" />
+            </div>
+            <div id={styles.backButton}>Back</div>
+          </div>
+          <div id={styles.gallery}>
+            {images.map((image, idx) => {
+              return (
+                // <div className={styles.galleryImage} key={idx} style={{backgroundImage: `url(${image})`}}></div>
+                <div
+                  key={idx}
+                  onClick={() => setIndex(idx)}
+                  id={styles.galleryImageHolder}
+                >
+                  <img
+                    className={
+                      index === idx
+                        ? styles.galleryImageactive
+                        : styles.galleryImage
+                    }
+                    src={image}
+                    alt="gallery"
+                  ></img>
+                </div>
+              );
+            })}
+          </div>
+          <div id={styles.showcase}>
+            <div>
+              <img
+                onClick={() => setIsModalOpen(true)}
+                id={styles.showcaseImage}
+                src={images[index]}
+                alt="showcase"
+              ></img>
+            </div>
+          </div>
+        </div>
+      </div>
+      {isModalOpen && (
+        <div id={styles.modalContainer} onClick={() => setIsModalOpen(false)}>
+          <img id={styles.modalImage} src={images[index]} alt="showcase"></img>
+        </div>
+      )}
+    </>
+  );
 }
--- front/src/pages/Listing.js
+++ front/src/pages/Listing.js
@@ -6,31 +6,36 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 export default function Listing() {
-    const [listing, setListing] = useState({})
+  const [listing, setListing] = useState({})
+  const [images, setImages] = useState('')
     const [result, setResult] = useState(false)
     let { HouseId } = useParams()
     console.log(HouseId)
-    useEffect(() => {
+  useEffect(() => {
+      result ? console.log('haha'):
       axios
         .post("http://localhost:4000/listing", { houseId: `${HouseId}` })
         .then((res) => {
-          console.log("result", res.data);
+          console.log("result", res.data[0].images);
           if (res.data === "failed") {
             setResult(false);
           } else {
-            setResult(true);
             setListing(res.data[0]);
+            setImages(res.data[0].images);
+            setResult(true);
+            
           }
         });
-    }, [HouseId]);
+    }, [HouseId, result]);
     
     
 
     return (
-        <>
-            <Nav />
-            <Gallery result={result} listing={listing} />
-            <Footer />
-        </>
-    )
+      <>
+        <Nav />
+        {result &&<Gallery images={images} />}
+
+        <Footer />
+      </>
+    );
 }
