b5f80f04855aa9cce20d83274543fc93010b87f3
perstraine <41206120+perstraine@users.noreply.github.com>
2022-09-22 12:19:35 +1200

    Listing details section done

--- front/src/pages/Listing.js
+++ front/src/pages/Listing.js
@@ -4,24 +4,33 @@ import Footer from "../components/shared/footer/Footer";
 import Gallery from "../components/listing/gallery/Gallery";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Details from "../components/listing/details/Details";
 
 export default function Listing() {
-  const [listing, setListing] = useState({})
   const [images, setImages] = useState('')
+  const [details, setDetails] = useState({})
+
     const [result, setResult] = useState(false)
     let { HouseId } = useParams()
     console.log(HouseId)
   useEffect(() => {
-      result ? console.log('haha'):
-      axios
+    result ? console.log('haha') :
+    axios
         .post("http://localhost:4000/listing", { houseId: `${HouseId}` })
         .then((res) => {
-          console.log("result", res.data[0].images);
+          console.log("result", res.data[0]);
           if (res.data === "failed") {
             setResult(false);
           } else {
-            setListing(res.data[0]);
             setImages(res.data[0].images);
+            const detailsPlaceHolder = { 'address':res.data[0].address };
+            detailsPlaceHolder.bed = res.data[0].bedrooms;
+            detailsPlaceHolder.bath = res.data[0].bathrooms;
+            detailsPlaceHolder.parking = res.data[0].parking;
+            detailsPlaceHolder.price = res.data[0].price;
+            setDetails(detailsPlaceHolder);
+
+            // setAddress(res.data[0].address);
             setResult(true);
             
           }
@@ -33,7 +42,12 @@ export default function Listing() {
     return (
       <>
         <Nav />
-        {result &&<Gallery images={images} />}
+        {result && (
+          <>
+            <Gallery images={images} />
+            <Details details={details} />
+          </>
+        )}
 
         <Footer />
       </>
