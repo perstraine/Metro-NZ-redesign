1e046655897870329cbbb81ee4a1c37095c1ba10
annalittler <77206754+annalittler@users.noreply.github.com>
2022-09-23 12:05:20 +1200

    for merge

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-<<<<<<< HEAD
+
 import Listings from "./pages/Listings";
 import Search from "./pages/Search";
+import ThirdPartyPage from "./pages/ThirdPartyPage";
 
 function App() {
   return (
@@ -13,27 +14,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/listings" element={<Listings />} />
+          <Route path="/third-party" element={<ThirdPartyPage />} />
         </Routes>
       </BrowserRouter>
     </>
   );
-=======
-// import Search from "./pages/Search";
-import ThirdPartyPage from "./pages/ThirdPartyPage";
-
-function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					{/* <Route path="/search" element={<Search />} /> */}
-					<Route path="/third-party" element={<ThirdPartyPage />} />
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
->>>>>>> development
 }
 
 export default App;
